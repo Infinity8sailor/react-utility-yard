f47f7e4eec1d365b454c9933f1f2ec9d2672ce86
Vaibhav Dasharathe <vinfinitysailor@gmail.com>
2023-10-27 21:42:45 +0530

    ts issue

--- src/components/Icons/icon.tsx
+++ src/components/Icons/icon.tsx
@@ -17,19 +17,19 @@ export const MaterialIcon = ({
   size = "md",
   cursor,
 }: Props) => {
-  const sizeStyles = [
-    { sm: "h-4 w-6", md: "h-6 w-10", lg: "h-8 w-14" },
-    {
-      sm: "m-0.5 h-3 w-3 peer-checked:start-2",
-      md: "m-1 h-4 w-4 peer-checked:start-4",
-      lg: "m-1 h-6 w-6 peer-checked:start-6",
-    },
-  ];
-  const colors = {
-    blue: `peer-checked:bg-blue-500`,
-    green: `peer-checked:bg-green-500`,
-    red: `peer-checked:bg-red-500`,
-  };
+  // const sizeStyles = [
+  //   { sm: "h-4 w-6", md: "h-6 w-10", lg: "h-8 w-14" },
+  //   {
+  //     sm: "m-0.5 h-3 w-3 peer-checked:start-2",
+  //     md: "m-1 h-4 w-4 peer-checked:start-4",
+  //     lg: "m-1 h-6 w-6 peer-checked:start-6",
+  //   },
+  // ];
+  // const colors = {
+  //   blue: `peer-checked:bg-blue-500`,
+  //   green: `peer-checked:bg-green-500`,
+  //   red: `peer-checked:bg-red-500`,
+  // };
   return (
     <span
       onClick={onClick}
