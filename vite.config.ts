d2297cf816c42ef1261c76e80a6866783d545bc0
Vaibhav Dasharathe <vinfinitysailor@gmail.com>
2024-02-03 11:06:54 +0530

    storybook calls vite config present in root by default

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-console.log("Im called");
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [react()],
 });
