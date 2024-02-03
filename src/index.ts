e8f9cdcac0a314bdd1a77e93b5852722eb47870b
Vaibhav Dasharathe <vinfinitysailor@gmail.com>
2024-02-03 11:03:15 +0530

    Reverted the react script changes

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// import "./tailwind.css";
+import 'material-icons/iconfont/material-icons.css';
+export * from "./components/index";
+export * from "./hooks/index";
+const Hello = () => console.log("Jelllo...");
+export { Hello };
